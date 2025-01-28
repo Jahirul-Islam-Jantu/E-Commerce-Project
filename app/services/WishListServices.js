cadaa45786fb09c83c326fddc4b04f9e05ce14e6
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-28 22:18:06 +0600

    wish list

--- app/services/WishListServices.js
+++ app/services/WishListServices.js
@@ -1,6 +1,71 @@
 import WishModel from "../model/WishModel.js";
 
-export const getWishlist = async (req) => {};
+export const getWishlist = async (req) => {
+  try {
+    let user_id = req.headers.user_id;
+    let matchStage = { $match: { userID: user_id } };
+
+    let JoinStageProduct = {
+      $lookup: {
+        from: "products",
+        localField: "productID",
+        foreignField: "_id",
+        as: "product",
+      },
+    };
+    let unwindProductStage = { $unwind: "$product" };
+
+    let JoinStageBrand = {
+      $lookup: {
+        from: "brands",
+        localField: "product.brandID",
+        foreignField: "_id",
+        as: "brand",
+      },
+    };
+    let unwindBrandStage = { $unwind: "$brand" };
+
+    let JoinStageCategory = {
+      $lookup: {
+        from: "categories",
+        localField: "product.categoryID",
+        foreignField: "_id",
+        as: "category",
+      },
+    };
+    let unwindCategoryStage = { $unwind: "$category" };
+
+    let projectionStage = {
+      $project: {
+        _id: 0,
+        userID: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        "product._id": 0,
+        "product.categoryID": 0,
+        "product.brandID": 0,
+        "brand._id": 0,
+        "category._id": 0,
+      },
+    };
+
+    let data = await WishModel.aggregate([
+      matchStage,
+      JoinStageProduct,
+      unwindProductStage,
+      JoinStageBrand,
+      unwindBrandStage,
+      JoinStageCategory,
+      unwindCategoryStage,
+      projectionStage,
+    ]);
+
+    return { status: "success", data: data };
+  } catch (e) {
+    console.log(e);
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
 export const saveToWishlistService = async (req) => {
   try {
