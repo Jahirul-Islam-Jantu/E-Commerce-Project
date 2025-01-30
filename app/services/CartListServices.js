dd512e75ab34673ed687c124d55338f3b099406c
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-30 09:46:26 +0600

    cart list shoq

--- app/services/CartListServices.js
+++ app/services/CartListServices.js
@@ -1,11 +1,76 @@
 import CartModel from "../model/CartModel.js";
 import mongoose from "mongoose";
 
-const ObjectID = mongoose.Types.ObjectId;
+const ObjID = mongoose.Types.ObjectId;
 
 export const CartListService = async (req) => {
   try {
-  } catch (err) {}
+    let user_id = new ObjID(req.headers.user_id); // req.headers.user_id;
+    let matchStage = { $match: { userID: user_id } };
+
+    let JoinStageProduct = {
+      $lookup: {
+        from: "products",
+        localField: "productID",
+        foreignField: "_id",
+        as: "product",
+      },
+    };
+    let unwindProductStage = { $unwind: "$product" };
+
+    let JoinStageBrand = {
+      $lookup: {
+        from: "brands",
+        localField: "product.brandID",
+        foreignField: "_id",
+        as: "brand",
+      },
+    };
+    let unwindBrandStage = { $unwind: "$brand" };
+
+    let JoinStageCategory = {
+      $lookup: {
+        from: "categories",
+        localField: "product.categoryID",
+        foreignField: "_id",
+        as: "category",
+      },
+    };
+    let unwindCategoryStage = { $unwind: "$category" };
+
+    let projectionStage = {
+      $project: {
+        _id: 0,
+        userID: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        "product._id": 0,
+        "product.categoryID": 0,
+        "product.brandID": 0,
+        "brand._id": 0,
+        "category._id": 0,
+      },
+    };
+
+    let data = await CartModel.aggregate([
+      matchStage,
+      JoinStageProduct,
+      unwindProductStage,
+      JoinStageBrand,
+      unwindBrandStage,
+      JoinStageCategory,
+      unwindCategoryStage,
+      projectionStage,
+    ]);
+
+    return { status: "success", data: data };
+  } catch (err) {
+    return {
+      status: "failed",
+      message: "Failed to get products from CartList",
+      error: err,
+    };
+  }
 };
 
 export const SaveCartListService = async (req) => {
